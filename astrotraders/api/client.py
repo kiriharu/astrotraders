838e73b61737b95b1c248979405a21e8385db306
Alexander-D-Karpov <alexandr.d.karpov@gmail.com>
2023-05-13 23:05:49 +0300

    made AstroTradersClient resources as property

--- astrotraders/api/client.py
+++ astrotraders/api/client.py
@@ -1,5 +1,3 @@
-from typing import TYPE_CHECKING, cast
-
 from httpx import Client
 
 from astrotraders.api.resources import (
@@ -17,11 +15,11 @@ class AstroTradersClient:
     def __init__(self, httpx_instance: Client):
         self._httpx_instance = httpx_instance
         self._client = HttpxClientWrapper(self._httpx_instance)
-        self.agents = AgentsResource(self._client)
-        self.systems = SystemsResource(self._client)
-        self.contracts = ContractsResource(self._client)
-        self.factions = FactionsResource(self._client)
-        self.fleet = FleetResource(self._client)
+        self._agents = AgentsResource(self._client)
+        self._systems = SystemsResource(self._client)
+        self._contracts = ContractsResource(self._client)
+        self._factions = FactionsResource(self._client)
+        self._fleet = FleetResource(self._client)
 
     @classmethod
     def set_up(
@@ -33,6 +31,45 @@ class AstroTradersClient:
             headers={"Authorization": f"Bearer {token}"},
         )
         return cls(client)
+
+    @property
+    def agents(self) -> AgentsResource:
+        """
+        Agents are the primary entity in SpaceTraders.
+        Player controls a single agent which can be used to manage a fleet of ships and conduct trade with factions
+        """
+        return self._agents
+
+    @property
+    def systems(self) -> SystemsResource:
+        """
+        Systems are the primary locations in the SpaceTraders universe.
+        Every system has a type, which is typically a type of star, and a set of x, y coordinates.
+        """
+        return self._systems
+
+    @property
+    def contracts(self) -> ContractsResource:
+        """
+        Faction contracts are a good way to earn credits and faction reputation.
+        Your contract will have a set of terms, which describe the requirements for completing the contract.
+        """
+        return self._contracts
+
+    @property
+    def factions(self) -> FactionsResource:
+        """
+        Factions are the primary NPC organizations in SpaceTraders.
+        Each faction will have a unique set of ships, contracts, and trade routes for you to explore.
+        """
+        return self._factions
+
+    @property
+    def fleet(self) -> FleetResource:
+        """
+        Fleet is the collection of your ships.
+        """
+        return self._fleet
 
     def close(self) -> "None":
         self._httpx_instance.close()
